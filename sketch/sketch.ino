28e9a289c201fd1b9f9f32afb979a55b912b68bc
sai <sai@mail.ru>
2026-04-29 20:34:59 +0700

    Добавлен фоторезистор в схему

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,6 +3,7 @@
 #define BUT 8
 #define LED 11
 #define BUZ 5
+#define PHOTO A0
 
 Button b(8);
 void setup() {
@@ -17,6 +18,8 @@ void setup() {
 uint32_t t, hand;
 void loop() {
   //tone(5, 3000, 500);
+ Serial.println(analogRead(PHOTO));
+ 
  static bool flag = false;
   if (b.pressed()){
     digitalWrite(LED, HIGH);
